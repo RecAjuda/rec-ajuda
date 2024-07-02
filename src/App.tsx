8273fd99dd8af1a5cb2671fbf026a1777e56bcbf
Setoue <proshigeru@gmail.com>
2024-07-01 23:50:19 -0300

    refactor: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Main } from "./pages/main/Main";
+import { FormLogin } from "./pages/login";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/login" element={<FormLogin />} />
     </Routes>
   );
 }
