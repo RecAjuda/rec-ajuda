278480f24ad00fdd6e79dd0d97c021ffc59d0c06
Setoue <proshigeru@gmail.com>
2024-07-05 13:56:55 -0300

    fix

--- src/components/PointMap/index.tsx
+++ src/components/PointMap/index.tsx
@@ -1,32 +1,31 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { PointMapType } from "./type";
-import { useEffect } from "react";
-
 
 export const PointMapComponent = ({
-    points,
-    centerMap,
-    zoom
+  points,
+  centerMap,
+  zoom,
 }: PointMapType) => {
-    return (
-        <>
-            <MapContainer className="relative z-10 h-screen w-screen" center={centerMap} zoom={zoom} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                {
-                    points.map(element => (
-                        <Marker  position={[element.latitude, element.longitude]}>
-                            <Popup className={`text-${element.color}`}>
-                                {`${element.title}-${element.openStatus}-${element.type}`}
-                            </Popup>
-                        </Marker>
-                    ))
-                }
-
-            </MapContainer>
-        </>
-    )
-}
-
+  return (
+    <>
+      <MapContainer
+        className="relative z-10 h-screen w-screen"
+        center={centerMap}
+        zoom={zoom}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {points.map((element) => (
+          <Marker position={[element.latitude, element.longitude]}>
+            <Popup className={`text-${element.color}`}>
+              {`${element.title}-${element.openStatus}-${element.type}`}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </>
+  );
+};
