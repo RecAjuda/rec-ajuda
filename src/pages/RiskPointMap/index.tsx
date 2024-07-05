5729243d7c846fd3ec7fed15fbc402d5b52723aa
Davi Freitas <davimello14@gmail.com>
2024-07-05 03:45:50 -0300

    [Refactor] set new points inside map and apply new center

--- src/pages/RiskPointMap/index.tsx
+++ src/pages/RiskPointMap/index.tsx
@@ -4,15 +4,64 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { PointMapComponent } from "../../components/PointMap";
 import { GeoLocation } from "../../components/PointMap/type";
 
-const DEFAULT_MAP_ZOOM = 16;
+const DEFAULT_MAP_ZOOM = 17;
 let geoLocations: GeoLocation[] = [{
     latitude: -8.001247,
     longitude: -34.927187,
-    description: "tester",
+    description: "Abrigo",
     color: "green"
-}];
+},
+{
+    latitude: -8.001733,
+    longitude: -34.928811,
+    description: "Ponto de Apoio",
+    color: "green"
+},
+{
+    latitude: -8.000799,
+    longitude: -34.929713,
+    description: "Abrigo",
+    color: "green"
+},
+{
+    latitude: -8.000968,
+    longitude: -34.928444,
+    description: "Ponto de Apoio",
+    color: "green"
+},
+{
+    latitude: -8.001454,
+    longitude: -34.928962,
+    description: "Ponto de Apoio",
+    color: "green"
+},
+{
+    latitude: -8.001436,
+    longitude: -34.928538,
+    description: "Abrigo",
+    color: "green"
+},
+{
+    latitude: -8.001845,
+    longitude: -34.929593,
+    description: "Abrigo",
+    color: "green"
+},
+{
+    latitude: -8.002312,
+    longitude: -34.932288,
+    description: "Ponto de Apoio",
+    color: "green"  
+},
+{
+    latitude: -8.002433,
+    longitude: -34.932709,
+    description: "Abrigo",
+    color: "green"
+}
+];
 
-const CENTER_MAP: [number, number] = [-8.0623551, -34.8775358]
+const CENTER_MAP: [number, number] = [-8.002312, -34.932288]
 
 export const RiskPointMap = () => {
     return (
