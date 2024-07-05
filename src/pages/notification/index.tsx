38375b53762979e8be96c558847cd8cde3a4e755
Setoue <proshigeru@gmail.com>
2024-07-05 13:48:55 -0300

    refactor: fix notification

--- src/pages/notification/index.tsx
+++ src/pages/notification/index.tsx
@@ -1,8 +1,5 @@
-import  {ModalNotif}  from "./ModalNotification";
-import  {ModalNotifBairro}  from "./ModalNotiBairros";
+import { ModalNotifBairro } from "./ModalNotiBairros";
 
 export const Notification = () => {
-  return (
-    <ModalNotifBairro />
-  );
+  return <ModalNotifBairro />;
 };
