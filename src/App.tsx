7f0186526654f0881a6b534758a4d75458e579f1
Setoue <proshigeru@gmail.com>
2024-07-05 00:01:22 -0300

    refactor: update url router

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ import { FormLogin } from "./pages/login";
 import { Map } from "./pages/map";
 import { Shelter } from "./pages/shelter";
 
+const URL_BASE = "/rec-ajuda-client";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" element={<FormLogin />} />
-      <Route path="/map" element={<Map />} />
-      <Route path="/shelter" element={<Shelter />} />
+      <Route path={`${URL_BASE}/`} element={<Main />} />
+      <Route path={`${URL_BASE}/login`} element={<FormLogin />} />
+      <Route path={`${URL_BASE}/map`} element={<Map />} />
+      <Route path={`${URL_BASE}/shelter`} element={<Shelter />} />
     </Routes>
   );
 }
