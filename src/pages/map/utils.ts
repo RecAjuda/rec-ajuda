f1020e07f7843c9129964a5784dfa1fa66421e7f
Setoue <proshigeru@gmail.com>
2024-07-04 23:48:50 -0300

    refactor: fix vite

---
+++ src/pages/map/utils.ts
@@ -0,0 +1,696 @@
+export const popsUps = [
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0461919, -34.919189],
+    },
+    properties: {
+      rating: 3.2,
+      restaurant_name: "Sonu Da Dhaba",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0461919, -34.919971],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "S.K Fast Food & Tiffin Centre",
+    },
+  },
+
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9475296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "U21 Urban Eats",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9475296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "V22 Village Diner",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9575296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "W23 Waterfront Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9375296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "X24 Crossroads Bistro",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0379757, -34.9475296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "Y25 Central Perk",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0479757, -34.9475296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "Z26 Zen Zone",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9525296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "A27 Avenue Grill",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9425296],
+    },
+    properties: {
+      rating: 3.6,
+      restaurant_name: "B28 Bistro Haven",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0229757, -34.9475296],
+    },
+    properties: {
+      rating: 4.5,
+      restaurant_name: "E31 Eatery Delight",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0629757, -34.9475296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "F32 Foodies' Hub",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9675296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "G33 Gourmet Street",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9275296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "H34 Heritage Diner",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9375296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "I35 Island Bistro",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9575296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "J36 Joyful Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9575296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "K37 Kitchen Corner",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9575296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "L38 Lovely Eats",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9475296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "U21 Urban Eats",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9475296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "V22 Village Diner",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9575296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "W23 Waterfront Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9375296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "X24 Crossroads Bistro",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0379757, -34.9475296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "Y25 Central Perk",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0479757, -34.9475296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "Z26 Zen Zone",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9525296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "A27 Avenue Grill",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9425296],
+    },
+    properties: {
+      rating: 3.6,
+      restaurant_name: "B28 Bistro Haven",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0379757, -34.9375296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "C29 Corner Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0379757, -34.9475296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "E31 Easy Bites",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0479757, -34.9475296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "F32 Food Palace",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9525296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "G33 Gourmet Garden",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9425296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "H34 Happy Meals",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9475296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "I35 Island Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9475296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "J36 Joyful Bites",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9375296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "K37 Kitchen Delight",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9575296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "L38 Lovely Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9375296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "M39 Metro Eats",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9575296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "N40 Nest Kitchen",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0479757, -34.9375296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "D30 Downtown Deli",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0529757, -34.9275296],
+    },
+    properties: {
+      rating: 3.6,
+      restaurant_name: "M39 Metro Deli",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0329757, -34.9275296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "N40 Nest Bistro",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0379757, -34.9375296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "C29 Corner Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0479757, -34.9375296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "D30 Downtown Deli",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9475296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "S.K Fast Food & Tiffin Centre",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0430757, -34.9475296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "K11 Kitchen",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9476296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "L12 Lounge",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0431757, -34.9474296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "M13 Munchies",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0430757, -34.9473296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "N14 Nook",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0432757, -34.9476296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "O15 Oasis",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0428757, -34.9474296],
+    },
+    properties: {
+      rating: 3.6,
+      restaurant_name: "P16 Place",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9472296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "Q17 Quick Bites",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0431757, -34.9476296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "R18 Rest Stop",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0432757, -34.9475296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "S19 Station",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0427757, -34.9476296],
+    },
+    properties: {
+      rating: 3.5,
+      restaurant_name: "T20 Table",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0431757, -34.9473296],
+    },
+    properties: {
+      rating: 4.2,
+      restaurant_name: "A1 Snack Bar",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0428757, -34.9476296],
+    },
+    properties: {
+      rating: 3.9,
+      restaurant_name: "B2 Bistro",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0430757, -34.9475296],
+    },
+    properties: {
+      rating: 4.0,
+      restaurant_name: "C3 Café",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0427757, -34.9474296],
+    },
+    properties: {
+      rating: 3.7,
+      restaurant_name: "D4 Diner",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9473296],
+    },
+    properties: {
+      rating: 4.1,
+      restaurant_name: "E5 Eatery",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0425757, -34.9475296],
+    },
+    properties: {
+      rating: 3.6,
+      restaurant_name: "F6 Food Court",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0432757, -34.9472296],
+    },
+    properties: {
+      rating: 4.3,
+      restaurant_name: "G7 Grill",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0426757, -34.9476296],
+    },
+    properties: {
+      rating: 3.8,
+      restaurant_name: "H8 Hot Spot",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0429757, -34.9474296],
+    },
+    properties: {
+      rating: 4.4,
+      restaurant_name: "I9 Inn",
+    },
+  },
+  {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [-8.0430757, -34.9476296],
+    },
+    properties: {
+      rating: 3.5,
+      restaurant_name: "J10 Joint",
+    },
+  },
+];
